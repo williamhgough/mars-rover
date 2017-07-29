05bf4d00af223cada30f3eb011cede2594cdc2e2
William Gough <williamhenrygough@gmail.com>
2017-07-29 10:11:31 +0100

    Add position reporting

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	grid := Grid{0, 0}
 	roverOne := Rover{Coords{0, 0}, ""}
@@ -8,4 +10,9 @@ func main() {
 	grid.Build()
 	grid.AddRover(&roverOne)
 	grid.AddRover(&roverTwo)
+
+	fmt.Printf("Rover One Position Report: %v %v %v\n", roverOne.Position.X,
+		roverOne.Position.Y, roverOne.Direction)
+	fmt.Printf("Rover Two Position Report: %v %v %v\n", roverTwo.Position.X,
+		roverTwo.Position.Y, roverTwo.Direction)
 }
